f9f3b622274ef771c3a215cb794dd0d5e1d18513
Abdallahi Nah <abdallahi.nah.un.fst@gmail.com>
2025-07-01 16:14:01 +0000

    some changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : "https://chat-app-ashy-eight-12.vercel.app/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
